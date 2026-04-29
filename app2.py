067bdb98319df0108f2c3a6b7069e87349100607
M Salman <hdslman04@gmail.com>
2026-04-29 14:40:26 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -27,7 +27,7 @@ st.markdown("""
     }
 
     .stMarkdown p {
-        font-size: 20px; /* Adjust body text size */
+        font-size: 30px; /* Adjust body text size */
     }
      
 .block-container {
